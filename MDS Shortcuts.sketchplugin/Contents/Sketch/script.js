bc0a5094f391d3e169346f749e2f4b9cd7d47489
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2017-04-13 20:01:07 +0200

    Support version 43 of Sketch

--- MDS Shortcuts.sketchplugin/Contents/Sketch/script.js
+++ MDS Shortcuts.sketchplugin/Contents/Sketch/script.js
@@ -9,7 +9,7 @@ var align = {
 
 function alignLayers(context, alignment) {
   var doc = context.document
-  var action = doc.actionsController().actionWithID(alignment);
+  var action = doc.actionsController().actionForID(alignment);
 
   if(action.validate()) {
       action.doPerformAction(nil);
@@ -41,4 +41,3 @@ function alignMiddle(context) {
 function alignBottom(context) {
   alignLayers(context, align.Bottom);
 }
-
